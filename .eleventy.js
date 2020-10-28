1a431d0c37ceccb852b1e31e5e2b279ae98e3114
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2020-10-28 11:15:37 +0700

    update webmention

--- .eleventy.js
+++ .eleventy.js
@@ -55,96 +55,15 @@ const markdownItAnchor = require("markdown-it-anchor");
 const localImages = require("./third_party/eleventy-plugin-local-images/.eleventy.js");
 const CleanCSS = require("clean-css");
 const GA_ID = require("./_data/metadata.json").googleAnalyticsId;
+const filters = require('./_11ty/filters')
+
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss)
-  eleventyConfig.addPlugin(pluginSyntaxHighlight)
-  eleventyConfig.setDataDeepMerge(true)
-
-  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk')
-
-  eleventyConfig.addFilter(
-    'readableDate',
-    (dateObj, format = 'dd LLL yyyy') => {
-      return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(format)
-    }
-  )
-
-  eleventyConfig.addFilter('dateFromTimestamp', (timestamp) => {
-    return DateTime.fromISO(timestamp, { zone: 'utc' }).toJSDate()
-  })
-
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toFormat('yyyy-LL-dd')
-  })
-
-  // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter('head', (array, n) => {
-    if (n < 0) {
-      return array.slice(n)
-    }
-
-    return array.slice(0, n)
-  })
-
-
-      // WEBMENTIONS FILTER
-  eleventyConfig.addFilter('webmentionsForUrl', (webmentions, url) => {
-    // define which types of webmentions should be included per URL.
-    // possible values listed here:
-    // https://github.com/aaronpk/webmention.io#find-links-of-a-specific-type-to-a-specific-page
-    const allowedTypes = ['mention-of', 'in-reply-to']
-
-    // define which HTML tags you want to allow in the webmention body content
-    // https://github.com/apostrophecms/sanitize-html#what-are-the-default-options
-    const allowedHTML = {
-      allowedTags: ['b', 'i', 'em', 'strong', 'a'],
-      allowedAttributes: {
-        a: ['href']
-      }
-    }
-
-    // clean webmention content for output
-    const clean = (entry) => {
-      const { html, text } = entry.content
-
-      if (html) {
-        // really long html mentions, usually newsletters or compilations
-        entry.content.value =
-          html.length > 2000
-            ? `mentioned this in <a href="${entry['wm-source']}">${entry['wm-source']}</a>`
-            : sanitizeHTML(html, allowedHTML)
-      } else {
-        entry.content.value = sanitizeHTML(text, allowedHTML)
-      }
-
-      return entry
-    }
-    
-
-    // sort webmentions by published timestamp chronologically.
-    // swap a.published and b.published to reverse order.
-    const orderByDate = (a, b) => new Date(b.published) - new Date(a.published)
-
-    // only allow webmentions that have an author name and a timestamp
-    const checkRequiredFields = (entry) => {
-      const { author, published } = entry
-      return !!author && !!author.name && !!published
-    }
-
-    // run all of the above for each webmention that targets the current URL
-    return webmentions
-      .filter((entry) => entry['wm-target'] === url)
-      .filter((entry) => allowedTypes.includes(entry['wm-property']))
-      .filter(checkRequiredFields)
-      .sort(orderByDate)
-      .map(clean)
-  })
-
-}
-
-module.exports = function (eleventyConfig) {
+    // Filters
+    Object.keys(filters).forEach(filterName => {
+      eleventyConfig.addFilter(filterName, filters[filterName])
+    })
+  
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
